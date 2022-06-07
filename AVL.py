dcb55f3a382bfb1cae9d55c0c3fe507be0f77f12
Fengax <857514.leofeng@gmail.com>
2022-06-07 22:55:21 +1000

    added VL tree deletion and fixed AVL insertion error

--- AVL.py
+++ AVL.py
@@ -128,44 +128,80 @@ def rl_rotation(node):
     return new_root
 
 
-def fix_tree(node, case):
+def fix_tree(parent, node, case):
+    if node is None:
+        return None
+
     if case == "ll":
-        # Check if left subtree is imbalanced
-        if get_height(node.left) > 2 and node.left.b_factor < -1 or node.left.b_factor > 1:
-            node.left = fix_tree(node.left, "ll")
-
-            return node
+        # Find the first subtree that is imbalanced
+        if abs(node.b_factor) > 1:
+            new = ll_rotation(node)
+            if parent is None:
+                return new
+            else:
+                if parent.left == node:
+                    parent.left = new
+                else:
+                    parent.right = new
         else:
-            return ll_rotation(node)
+            fix_tree(node, node.left, "ll")
+            fix_tree(node, node.right, "ll")
     elif case == "lr":
-        # Check if left subtree is imbalanced
-        if get_height(node.left) > 2 and node.left.b_factor < -1 or node.left.b_factor > 1:
-            node.left = fix_tree(node.left, "lr")
-
-            return node
+        # Find the first subtree that is imbalanced
+        if abs(node.b_factor) > 1:
+            new = lr_rotation(node)
+            if parent is None:
+                return new
+            else:
+                if parent.left == node:
+                    parent.left = new
+                else:
+                    parent.right = new
         else:
-            return lr_rotation(node)
+            fix_tree(node, node.left, "lr")
+            fix_tree(node, node.right, "lr")
     elif case == "rr":
-        # Check if right subtree is imbalanced
-        if get_height(node.right) > 2 and node.right.b_factor < -1 or node.right.b_factor > 1:
-            node.right = fix_tree(node.right, "rr")
-
-            return node
+        # Find the first subtree that is imbalanced
+        if abs(node.b_factor) > 1:
+            new = rr_rotation(node)
+            if parent is None:
+                return new
+            else:
+                if parent.left == node:
+                    parent.left = new
+                else:
+                    parent.right = new
         else:
-            return rr_rotation(node)
+            fix_tree(node, node.left, "rr")
+            fix_tree(node, node.right, "rr")
     elif case == "rl":
-        # Check if right subtree is imbalanced
-        if get_height(node.right) > 2 and node.right.b_factor < -1 or node.right.b_factor > 1:
-            node.right = fix_tree(node.right, "rl")
-
-            return node
+        # Find the first subtree that is imbalanced
+        if abs(node.b_factor) > 1:
+            new = rl_rotation(node)
+            if parent is None:
+                return new
+            else:
+                if parent.left == node:
+                    parent.left = new
+                else:
+                    parent.right = new
         else:
-            return rl_rotation(node)
+            fix_tree(node, node.left, "rl")
+            fix_tree(node, node.right, "rl")
     else:
         raise Exception("Invalid imbalance case")
 
 
 def insert(node, value):
+    def check_imbalance(node):
+        if node is None:
+            return False
+
+        if node.b_factor < -1 or node.b_factor > 1:
+            return True
+        else:
+            return check_imbalance(node.left) or check_imbalance(node.right)
+
     print(f"Inserting {value}...")
     queue = []
 
@@ -179,30 +215,36 @@ def insert(node, value):
         get_b_factor(node)
 
         # Tree is imbalanced
-        if node.b_factor < -1 or node.b_factor > 1:
+        if check_imbalance(node):
             print("Tree looks like this currently. This tree requires rebalancing:")
             print_tree(node, val="value")
             print(f"Here are the balance factors of the tree")
             print_tree(node, val="b_factor")
             # Left-left imbalance
             if queue[0] == "left" and queue[1] == "left":
-                new_root = fix_tree(node, "ll")
+                new_root = fix_tree(None, node, "ll")
             elif queue[0] == "left" and queue[1] == "right":
                 # Left-right imbalance
-                new_root = fix_tree(node, "lr")
+                new_root = fix_tree(None, node, "lr")
             elif queue[0] == "right" and queue[1] == "right":
                 # Right-right imbalance
-                new_root = fix_tree(node, "rr")
+                new_root = fix_tree(None, node, "rr")
             elif queue[0] == "right" and queue[1] == "left":
                 # Right-left imbalance
-                new_root = fix_tree(node, "rl")
+                new_root = fix_tree(None, node, "rl")
             else:
                 raise Exception("Invalid imbalance case")
 
-            print("Tree after rebalancing:")
-            print_tree(new_root, val="value")
-            print(DELIMITER)
-            return new_root
+            if new_root is not None:
+                print("Tree after rebalancing:")
+                print_tree(new_root, val="value")
+                print(DELIMITER)
+                return new_root
+            else:
+                print("Tree after rebalancing:")
+                print_tree(node, val="value")
+                print(DELIMITER)
+                return node
         else:
             print("Tree is balanced")
             print_tree(node, val="value")
@@ -210,14 +252,121 @@ def insert(node, value):
             return node
 
 
+def get_max(node):
+    if node is None:
+        return float('-inf')
+
+    return max(node.value, get_max(node.left), get_max(node.right))
+
+
+def recursive_delete(parent, node, value):
+    if node.value == value:
+        # Leaf node, no children
+        if node.left is None and node.right is None:
+            if parent is not None:
+                if parent.left == node:
+                    parent.left = None
+                else:
+                    parent.right = None
+            else:
+                # Deleting root node
+                node.value = None
+        elif node.left is None and node.right is not None:
+            if parent is None:
+                node.value = node.right.value
+                node.right = None
+            else:
+                parent.right = node.right
+        elif node.left is not None and node.right is None:
+            if parent is None:
+                node.value = node.left.value
+                node.left = None
+            else:
+                parent.left = node.left
+        else:
+            left_max = get_max(node.left)
+            node.value = left_max
+            recursive_delete(node, node.left, left_max)
+    else:
+        if node.left is not None:
+            recursive_delete(node, node.left, value)
+
+        if node.right is not None:
+            recursive_delete(node, node.right, value)
+
+
+def delete(node, value):
+    print(f"Deleting {value}...")
+
+    def detect_case(node):
+        if node is None:
+            return False
+
+        if node.b_factor < -1 or node.b_factor > 1:
+            # Detect which case it is
+            if node.left is not None and node.left.left is not None:
+                return "ll"
+            elif node.right is not None and node.right.right is not None:
+                return "rr"
+            elif node.left is not None and node.left.right is not None:
+                return "lr"
+            elif node.right is not None and node.right.left is not None:
+                return "rl"
+            else:
+                raise Exception("Error detecting imbalance case for deletion")
+        else:
+            return detect_case(node.left) or detect_case(node.right)
+
+    recursive_delete(None, node, value)
+
+    # Check if root is not None (tree is empty)
+    if node.value is not None:
+        get_b_factor(node)
+
+        case = detect_case(node)
+        if not case:
+            print("Tree is still balanced after deletion balanced")
+            print_tree(node, val="value")
+            print(DELIMITER)
+            return node
+        else:
+            print("Tree is imbalanced after deletion. It is a {} imbalance case. Tree is like this: ".format(case))
+            print_tree(node, val="value")
+            print("Fixing tree...")
+
+            new_root = fix_tree(None, node, case)
+
+            print("Tree fixed. Tree is like this:")
+            print_tree(node, val="value")
+            print(DELIMITER)
+
+            if new_root is not None:
+                return new_root
+            else:
+                return node
+    else:
+        return None
+
+
 def insert_avl_tree(array):
     '''
     Inserts an array of values into an AVL tree.
     :param array: Array of (preferably) integers, but can handle any arbitrary data type
-    :return: None
+    :return: Root node of the AVL tree
     '''
     root = None
     for value in array:
         root = insert(root, value)
 
     return root
+
+
+def delete_avl_tree(root, array):
+    '''
+    Deletes an array of values from an AVL tree.
+    :param root: Root node of the tree
+    :param array: Array of (preferably) integers, but can handle any arbitrary data type
+    :return: Root node of the AVL tree
+    '''
+    for value in array:
+        root = delete(root, value)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from AVL import insert_avl_tree
+from AVL import insert_avl_tree, delete_avl_tree
 from twothreetree import insert_23_tree
-from utils import print_23_tree
 
 if __name__ == '__main__':
-    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-    # root = insert_avl_tree(data)
-    root = insert_23_tree(data)
+    data = [1,2,3,4,5,6,7,8,9,10,11]
+    root = insert_avl_tree(data)
+
+    to_delete = [5,6,7,8,9,10,11]
+    root = delete_avl_tree(root, to_delete)
+
+    #root = insert_23_tree(data)
+
